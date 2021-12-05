54a08f152a6104dc206bf8b6a96a4d5807608729
Ameer Hamza <ameernormie@gmail.com>
2021-12-05 14:09:12 +0500

    refactor: add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 import { DataRow, Search } from "./components";
 import "./styles/base.scss"
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080")
+      .then((res) => console.log("res ", res))
+      .catch((err) => console.log("err ", err));
+  }, []);
+
   return (
     <div className="App">
       <Search />
